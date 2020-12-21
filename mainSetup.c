8139e5b1cc256e9575a8761e496cb3fb9333f01f
Tuna <tunacinsoy@gmail.com>
2020-12-22 02:47:51 +0300

    21 aralik basladik ins

--- mainSetup.c
+++ mainSetup.c
@@ -1,25 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
- 
-#define MAX_LINE 80 /* 80 chars per line, per command, should be enough. */
- 
+#include <fcntl.h>
+#include <sys/types.h>
+
+#define MAX_LINE 128 /* 128 chars per line, per command, should be enough. */
+
+void setup(char inputBuffer[], char *args[], int *background);
+char *getPath(char *arg, char *envPath);
+int getCommandType(char *args[]);
+
+struct processNode
+{
+    char *name;
+    pid_t pid;
+    pid_t parent_pid;
+    int status; // 0 done // 1 working // 2 not process, command prompt
+    struct processNode *next;
+};
+struct processNode *root;
+
+char *envPath;
+
+int main(void)
+{
+    char inputBuffer[MAX_LINE];   /*buffer to hold command entered */
+    int background;               /* equals 1 if a command is followed by '&' */
+    char *args[MAX_LINE / 2 + 1]; /*command line arguments */
+
+    envPath = getenv("PATH");
+    while (1)
+    {
+        background = 0;
+        printf("myshell: ");
+        fflush(stdout);
+        /*setup() calls exit() when Control-D is entered */
+        setup(inputBuffer, args, &background);
+
+        execv(getPath(args[0], envPath), args);
+
+        /** the steps are:
+        (1) fork a child process using fork() */
+
+        /*
+                        (2) the child process will invoke execv()
+						(3) if background == 0, the parent will wait,
+                        otherwise it will invoke the setup() function again. */
+    }
+}
+
 /* The setup function below will not return any value, but it will just: read
 in the next command line; separate it into distinct arguments (using blanks as
 delimiters), and set the args array entries to point to the beginning of what
 will become null-terminated, C-style strings. */
 
-void setup(char inputBuffer[], char *args[],int *background)
+void setup(char inputBuffer[], char *args[], int *background)
 {
     int length, /* # of characters in the command line */
         i,      /* loop index for accessing inputBuffer array */
         start,  /* index where beginning of next command parameter is */
         ct;     /* index of where to place the next parameter into args[] */
-    
+
     ct = 0;
-        
+
     /* read what the user enters on the command line */
-    length = read(STDIN_FILENO,inputBuffer,MAX_LINE);  
+    length = read(STDIN_FILENO, inputBuffer, MAX_LINE);
 
     /* 0 is the system predefined file descriptor for stdin (standard input),
        which is the user's screen in this case. inputBuffer by itself is the
@@ -29,70 +76,81 @@ void setup(char inputBuffer[], char *args[],int *background)
 
     start = -1;
     if (length == 0)
-        exit(0);            /* ^d was entered, end of user command stream */
+        exit(0); /* ^d was entered, end of user command stream */
 
-/* the signal interrupted the read system call */
-/* if the process is in the read() system call, read returns -1
+    /* the signal interrupted the read system call */
+    /* if the process is in the read() system call, read returns -1
   However, if this occurs, errno is set to EINTR. We can check this  value
   and disregard the -1 value */
-    if ( (length < 0) && (errno != EINTR) ) {
+    if ((length < 0) && (errno != EINTR))
+    {
         perror("error reading the command");
-	exit(-1);           /* terminate with error code of -1 */
+        exit(-1); /* terminate with error code of -1 */
     }
 
-	printf(">>%s<<",inputBuffer);
-    for (i=0;i<length;i++){ /* examine every character in the inputBuffer */
+    printf(">>%s<<", inputBuffer);
+    for (i = 0; i < length; i++)
+    { /* examine every character in the inputBuffer */
 
-        switch (inputBuffer[i]){
-	    case ' ':
-	    case '\t' :               /* argument separators */
-		if(start != -1){
-                    args[ct] = &inputBuffer[start];    /* set up pointer */
-		    ct++;
-		}
-                inputBuffer[i] = '\0'; /* add a null char; make a C string */
-		start = -1;
-		break;
-
-            case '\n':                 /* should be the final char examined */
-		if (start != -1){
-                    args[ct] = &inputBuffer[start];     
-		    ct++;
-		}
-                inputBuffer[i] = '\0';
-                args[ct] = NULL; /* no more arguments to this command */
-		break;
-
-	    default :             /* some other character */
-		if (start == -1)
-		    start = i;
-                if (inputBuffer[i] == '&'){
-		    *background  = 1;
-                    inputBuffer[i-1] = '\0';
-		}
-	} /* end of switch */
-     }    /* end of for */
-     args[ct] = NULL; /* just in case the input line was > 80 */
-
-	for (i = 0; i <= ct; i++)
-		printf("args %d = %s\n",i,args[i]);
-} /* end of setup routine */
- 
-int main(void)
-{
-            char inputBuffer[MAX_LINE]; /*buffer to hold command entered */
-            int background; /* equals 1 if a command is followed by '&' */
-            char *args[MAX_LINE/2 + 1]; /*command line arguments */
-            while (1){
-                        background = 0;
-                        printf("myshell: ");
-                        /*setup() calls exit() when Control-D is entered */
-                        setup(inputBuffer, args, &background);
-                       
-                        /** the steps are:
-                        (1) fork a child process using fork()
-                        (2) the child process will invoke execv()
-						(3) if background == 0, the parent will wait,
-                        otherwise it will invoke the setup() function again. */
+        switch (inputBuffer[i])
+        {
+        case ' ':
+        case '\t': /* argument separators */
+            if (start != -1)
+            {
+                args[ct] = &inputBuffer[start]; /* set up pointer */
+                ct++;
             }
+            inputBuffer[i] = '\0'; /* add a null char; make a C string */
+            start = -1;
+            break;
+
+        case '\n': /* should be the final char examined */
+            if (start != -1)
+            {
+                args[ct] = &inputBuffer[start];
+                ct++;
+            }
+            inputBuffer[i] = '\0';
+            args[ct] = NULL; /* no more arguments to this command */
+            break;
+
+        default: /* some other character */
+            if (start == -1)
+                start = i;
+            if (inputBuffer[i] == '&')
+            {
+                *background = 1;
+                inputBuffer[i - 1] = '\0';
+            }
+        }            /* end of switch */
+    }                /* end of for */
+    args[ct] = NULL; /* just in case the input line was > 80 */
+
+    for (i = 0; i <= ct; i++)
+        printf("args %d = %s\n", i, args[i]);
+} /* end of setup routine */
+
+char *getPath(char *arg, char *envPath)
+{
+    char *temp = envPath;
+    char *ch;
+
+    ch = strtok(temp, ":");
+
+    while (ch != NULL)
+    {
+        char tempPath[200];
+        strcpy(tempPath, ch);
+        tempPath[strlen(ch)] = '/';
+        tempPath[strlen(ch) + 1] = '\0';
+        strcat(tempPath, arg);
+        if (access(tempPath, X_OK) != -1)
+        {
+            return strdup(tempPath);
+        }
+        ch = strtok(NULL, ":");
+    }
+
+    return NULL;
 }
